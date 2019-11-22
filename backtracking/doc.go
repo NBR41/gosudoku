fefea0891e0f660e836298a8c58ac1cd65705672
fabien.bernard <fabien.bernard@treezor.com>
2019-11-22 11:14:40 +0100

    try github CI

--- backtracking/doc.go
+++ backtracking/doc.go
@@ -1,4 +1,5 @@
 // Package backtracking is a package that provides :
 // - a structure to declare sudoku grids
 // - a function to resolve a sudoku grid
+// The algorithm of resolution is backtracking https://en.wikipedia.org/wiki/Backtracking
 package backtracking
